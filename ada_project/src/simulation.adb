b5a3bc79323a2dd089c91d156226b2183c770425
Seredvnskv <140017723+Seredvnskv@users.noreply.github.com>
2024-10-19 19:34:16 +0200

    Dodano punkt 5 furious worker

--- ada_project/src/simulation.adb
+++ ada_project/src/simulation.adb
@@ -1,6 +1,8 @@
--- RESTAURACJA
+-- SYMULACJA RESTAURACJI GRUPA ADA024
 
--- SAMBOR SEREDYNSKI 198035
+-- SAMBOR SEREDYNSKI
+-- MACIEJ KOWALCZYK 
+-- ZOFIA KRZESZOWIEC
 
 with Ada.Text_IO; use Ada.Text_IO;
 with Ada.Characters.Latin_1; use Ada.Characters.Latin_1;
@@ -38,17 +40,23 @@ procedure Simulation is
       entry Start(Consumer_Number: in Consumer_Type;
                   Consumption_Time: in Integer);
    end Consumer;
-
+   
+   task type Furious_Worker is
+      entry Start;
+   end Furious_Worker; 
+   
    -- Buffer receives dishes from producers and serves orders to consumers
    task type Buffer is
       -- Accept a dish into the kitchen (if there's enough space)
       entry Take(Product: in Producer_Type; Number: in Integer);
       -- Serve an order (if enough dishes are available)
       entry Deliver(Assembly: in Assembly_Type; Number: out Integer);
+      entry Quarrel_In_Storage;
    end Buffer;
 
    P: array ( 1 .. Number_Of_Producers ) of Producer;
    K: array ( 1 .. Number_Of_Consumers ) of Consumer;
+   FW: Furious_Worker;
    B: Buffer;
 
    ----TASK DEFINITIONS----
@@ -146,6 +154,31 @@ begin
    end loop;
 end Consumer;
 
+   -- Furious Worker 
+   
+   task body Furious_Worker is
+      subtype Fury_Level_Range is Integer range 0 .. 10;
+      package Random_Fury is new Ada.Numerics.Discrete_Random(Fury_Level_Range);
+      G: Random_Fury.Generator;
+      Fury_Level: Integer;
+   begin
+      accept Start do 
+         Random_Fury.Reset(G);
+      end Start; 
+   
+      loop 
+         delay 5.0;  -- Czas pracy pracownika
+         Fury_Level := Random_Fury.Random(G);
+         Put_Line(ESC & "[35m" & "FW: Furia Jana osiaga poziom " & Integer'Image(Fury_Level) & ESC & "[0m");
+         
+         if Fury_Level > 8 then
+            Put_Line(ESC & "[35m" & "FW: Janek jest wsciekly, zaraz zacznie rzucac talerzami!" & ESC & "[0m");
+            B.Quarrel_In_Storage;
+         end if;
+      end loop;
+   end Furious_Worker;
+   
+   
 --Buffer--
 
 task body Buffer is
@@ -159,7 +192,18 @@ task body Buffer is
    Max_Assembly_Content: array(Producer_Type) of Integer;
    Assembly_Number: array(Assembly_Type) of Integer := (1, 1, 1);
    In_Storage: Integer := 0;
-
+   
+   procedure Throwing_Products is
+   begin
+         for W in Producer_Type loop
+          In_Storage := In_Storage - (Storage(W) - (Storage(W) / 2));
+          Storage(W) := Storage(W) / 2;
+          Put_Line(ESC & "[35m" & "B: Stan kuchni po furii " & Integer'Image(Storage(W)) & " " & Product_Name(W) & ESC & "[0m");
+      end loop;
+         Put_Line(ESC & "[35m" & "B: Po furii Jana stracilismy polowe produktow w kuchni!" & ESC & "[0m");
+         Put_Line(ESC & "[35m" & "| Liczba dan w kuchni po furri: " & Integer'Image(In_Storage) & ESC & "[0m");
+   end Throwing_Products;   
+      
    procedure Setup_Variables is
    begin
       for W in Producer_Type loop
@@ -258,6 +302,12 @@ begin
          end Deliver;
          
       or
+         accept Quarrel_In_Storage do
+            Put_Line(ESC & "[35m" & "B: Rozpoczyna sie klotnia w kuchnii..." & ESC & "[0m");
+            Throwing_Products;
+         end Quarrel_In_Storage;   
+            
+      or
          delay 1.0;
          
       end select;
@@ -272,4 +322,5 @@ begin
    for J in 1 .. Number_Of_Consumers loop
       K(J).Start(J,12);
    end loop;
+   FW.Start;
 end Simulation;
